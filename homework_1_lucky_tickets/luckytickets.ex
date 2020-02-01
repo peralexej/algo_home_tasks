38d353c77bbdff99510d3b90f4e17d524be9a48c
alekseiperov <Aleksei.Perov@leongaming.com>
2020-02-01 23:00:34 +0200

    upd

--- homework_1_lucky_tickets/luckytickets.ex
+++ homework_1_lucky_tickets/luckytickets.ex
@@ -1,5 +1,7 @@
 defmodule LuckyTickets do
-  def calculate do
-    Enum.count(for a <- 0..9, b <- 0..9, c <- 0..9, d <- 0..9, e <- 0..9, f <- 0..9, a + b + c === d + e + f, do: a)
+  def calculate_by_loop do
+    Enum.count(for a <- 0..9, b <- 0..9, c <- 0..9, d <- 0..9, e <- 0..9, f <- 0..9, a + b + c === d + e + f, do: [])
   end
+
+
 end
